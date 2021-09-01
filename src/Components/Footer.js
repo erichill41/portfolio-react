a9b0f5007f207d8f1bb8634b2877b9583888b453
erichill41 <ehill4182@yahoo.com>
2021-09-01 13:01:34 -0600

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// learn how to make footer stay at bottom of react page
+
+
+function Footer() {
+    return (
+        <div className="container-fluid bg-dark fixed-bottom"> 
+            <p className="text-white"> Created by Eric </p>
+        </div>
+       
+    );
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div className="container-fluid shadow-lg p-3 mb-3 bg-dark rounded">
+            <header className="jumbotron">
+                <div className="container text-white">
+                    <h1 className="display-4 pb-2"> Eric Hill | <small className="text-muted"> Software Engineer </small> </h1>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
